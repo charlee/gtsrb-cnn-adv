bd7a91ec41e3f1a8c893adad72ea765187a04480
Charlee Li <oda.charlee@gmail.com>
2017-11-12 17:59:36 -0500

    added fgsm crafting

--- gtsrb_fgsm_batch_crafting.py
+++ gtsrb_fgsm_batch_crafting.py
@@ -2,36 +2,39 @@ import logging
 import os
 import numpy as np
 import tensorflow as tf
-from cnn_gtsrb.dataset.mnist import MnistProvider
+from cnn_gtsrb.dataset.gtsrb import GtsrbProvider
 from cnn_gtsrb.cnn.model import CNNModel
 
 from cnn_gtsrb.attacks.crafting import BatchFGSMCrafting
+
 
 DIR_NAME = os.path.join('tmp', 'batch_adv')
 if not os.path.isdir(DIR_NAME):
     os.makedirs(DIR_NAME)
 
+
 logging.basicConfig(level=logging.INFO)
 tf.logging.set_verbosity(tf.logging.INFO)
 
-fgsm_params = {'eps': 0.2, 'clip_min': 0., 'clip_max': 1.}
 
-mnist = MnistProvider()
-# mnist.dump_images()
+gtsrb = GtsrbProvider(ignore_small=True)
 
+# Make CNN Model
 cnn = CNNModel(
-    image_size=mnist.IMAGE_SIZE,
-    classes=mnist.CLASSES,
-    model_name='mnist-28x28.1',
-    model_dir='tmp/mnist_model-28x28.1',
-    conv_layers=[32, 64],
-    fc_layer=1024,
+    image_size=gtsrb.IMAGE_SIZE,
+    classes=gtsrb.CLASSES,
+    model_name='gtsrb-32x32',
+    model_dir='tmp/gtsrb_model-32x32',
+    conv_layers=[32, 64, 128],
+    fc_layer=512,
 )
 
-test_data = mnist.raw_test_data()
+fgsm_params = {'eps': 0.2, 'clip_min': 0., 'clip_max': 1.}
+
+test_data = gtsrb.raw_test_data()
 test_data = test_data[:5000]
 
-fgsm_crafting = BatchFGSMCrafting(cnn, fgsm_params, mnist.IMAGE_SIZE, mnist.CLASSES)
+fgsm_crafting = BatchFGSMCrafting(cnn, fgsm_params, gtsrb.IMAGE_SIZE, gtsrb.CLASSES)
 
 
 cnn.start_session()
@@ -42,11 +45,12 @@ for eps in np.arange(0.1, 0.6, 0.1):
     fgsm_crafting.update_params({'eps': eps})
 
     for batch_pos in range(0, test_data.shape[0], BATCH_SIZE):
+
         print("================ eps = {}, batch = {} =============".format(eps, batch_pos))
         batch = test_data[batch_pos:batch_pos+BATCH_SIZE]
         result = fgsm_crafting.craft_examples(batch)
         result = fgsm_crafting.summarize(batch, *result)
 
-        result.dump(os.path.join(DIR_NAME, 'fgsm_mnist-{:0.2f}-{}.npy'.format(eps, batch_pos)))
+        result.dump(os.path.join(DIR_NAME, 'fgsm_gtsrb-{:0.1f}-{}.npy'.format(theta, batch_pos)))
 
 cnn.end_session()
