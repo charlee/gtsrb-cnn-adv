803e3c67b343cea601e3ebc8bb14fa1744a93461
Charlee Li <oda.charlee@gmail.com>
2017-11-23 22:13:16 -0500

    added fasion mnist

---
+++ cnn_gtsrb/dataset/fashion_mnist.py
@@ -0,0 +1,11 @@
+from cnn_gtsrb import settings
+from .mnist import MnistProvider
+
+
+class FashionMnistProvider(MnistProvider):
+    URL_BASE = 'http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/'
+    TRAIN_IMAGES = 'train-images-idx3-ubyte.gz'
+    TRAIN_LABELS = 'train-labels-idx1-ubyte.gz'
+    TEST_IMAGES = 't10k-images-idx3-ubyte.gz'
+    TEST_LABELS = 't10k-labels-idx1-ubyte.gz'
+    DATA_DIR = settings.DATA_TEMP_FASHION_MNIST
