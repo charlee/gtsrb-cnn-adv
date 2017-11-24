326c4b780001e6e63b7464b831a79e05d0b016de
Charlee Li <oda.charlee@gmail.com>
2017-11-23 20:21:02 -0500

    added cifar cnn training

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ from cnn_gtsrb.cnn.model import CNNModel
 
 def train_cnn_cgtsrb10():
     gtsrb = ColorGtsrb10Provider()
-    # gtsrb.dump_images()
+    gtsrb.dump_images()
 
     cnn = CNNModel(
         image_size=gtsrb.IMAGE_SIZE,
@@ -32,16 +32,33 @@ def train_cnn_cgtsrb10():
     cnn.end_session()
 
 
-def download_cifar10():
+def train_cnn_cifar10():
     cifar10 = Cifar10Provider()
-    cifar10.dump_images()
+    #cifar10.dump_images()
+
+    cnn = CNNModel(
+        image_size=cifar10.IMAGE_SIZE,
+        classes=cifar10.CLASSES,
+        channels=3,
+        model_name='cifar10-32x32',
+        model_dir='tmp/cifar10_model-32x32',
+        conv_layers=[32, 64, 128],
+        fc_layer=512,
+    )
+
+    x, y = cnn.make_inputs()
+    probs = cnn.make_model(x)
+
+    cnn.start_session()
+    cnn.train(probs, x, y, 20000, cifar10)
+    # cnn.test(gtsrb)
+    cnn.end_session()
+
 
 
 if __name__ == '__main__':
-    download_cifar10()
-    exit()
     cmd = sys.argv[1]
     if cmd == 'train_cnn_cgtsrb10':
         train_cnn_cgtsrb10()
-    elif cmd == 'download_cifar10':
-        download_cifar10()
+    elif cmd == 'train_cnn_cifar10':
+        train_cnn_cifar10()
