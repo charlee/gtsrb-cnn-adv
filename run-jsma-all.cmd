2df3757d19b7cc5dea3ffbdb4da4c04d6abf682f
Charlee Li <oda.charlee@gmail.com>
2017-11-24 22:48:41 -0500

    wip

--- run-jsma-all.cmd
+++ run-jsma-all.cmd
@@ -1,7 +1,7 @@
-python run.py adv_fgsm cgtsrb10
-python run.py adv_fgsm fmnist
-python run.py adv_fgsm mnistbg
-python run.py adv_fgsm cifar10
+REM python run.py adv_fgsm cgtsrb10
+REM python run.py adv_fgsm fmnist
+REM python run.py adv_fgsm mnistbg
+REM python run.py adv_fgsm cifar10
 python run.py adv_fast_jsma cgtsrb10
 python run.py adv_fast_jsma fmnist
 python run.py adv_fast_jsma mnistbg
